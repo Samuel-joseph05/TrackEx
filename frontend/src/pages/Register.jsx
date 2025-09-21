140243ede3e702e621d45fda02c2a7f5b98c4a3c
Samuel-joseph05 <samuela05022004@gmail.com>
2025-09-21 13:44:56 +0530

    feat(auth): add user registration form with API integration and navigation

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,11 +1,86 @@
-import React from 'react'
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { API_URL } from "../config.js";
 
-function Register() {
+function Register() 
+
+{
+const[name,setName]=useState("")
+ const [email, setEmail] = useState("");
+const [password, setPassword] = useState("");
+const navigate= useNavigate();
+
+const handleSubmit =(e)=>{
+  e.preventDefault();
+  axios
+  .post(`${API_URL}/register`,{name,email,password})// API_URL is imported from config.js 
+  .then(res=>{
+    console.log(res.data)
+    navigate("/login");
+  })
+   .catch((err) =>console.log(err))
+}
   return (
-    <div>
-      
-    </div>
+    <>
+        <div className="flex justify-center  ">
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col gap-3  shadow-xl p-5 rounded-3xl w-100 m-15 "
+        >
+          <h1 className="text-2xl p-1">Register</h1>
+          <label htmlFor="username">Name</label>
+          <input
+            type="text"
+            placeholder="Enter your name"
+            className="p-2 bg-gray-100 rounded-2xl outline-none"
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+         required  ></input>
+
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            placeholder="Enter your gmail"
+            className="p-2 bg-gray-100 rounded-2xl outline-none"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+        required   />
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            placeholder="Enter your password "
+            className="p-2 bg-gray-100 rounded-2xl outline-none"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+        required   />
+          <p className="text-sm">
+            New here?
+            <br></br> Click Sign Up to create your account.
+          </p>
+          <button
+            className="text-white bg-purple-600 p-2  rounded-2xl transition duration-300 
+              ease-in-out  hover:bg-purple-700 hover:text-white"
+            type="submit"
+          >
+            Sign Up
+          </button>
+          <p className="">
+            Already have an account? Click Sign In to log in.
+          </p>
+          <button
+            className="bg-gray-300 text-black p-2 rounded-2xl transition duration-300 
+            ease-in-out  hover:bg-sky-400 hover:text-white"
+          >
+            Login
+          </button>
+        </form>
+      </div>
+    </>
   )
 }
 
-export default Register
+export default Register;
