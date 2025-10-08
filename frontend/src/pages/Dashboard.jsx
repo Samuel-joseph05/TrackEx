bf052a7d30fff87f795c45e04b78fac84ded05ba
Samuel-joseph05 <samuela05022004@gmail.com>
2025-10-08 12:02:17 +0530

    fix(ui): refactor Dashboard component layout and improve user greeting

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,16 +1,22 @@
-import { API_URL } from "../config";
+import ExpenseForm from "../components/ExpenseForm";
 
-// import axios from "axios";
-// import { useNavigate } from "react-router-dom";
+import LogOut from "./LogOut";
 function Dashboard() {
-   
-
 
   return (
     <div>
-      <h1>welcome user :</h1>
+      <div className="p-6 max-w-4xl mx-auto">
+        <h1 className="flex justify-center text-4xl mb-7 font-serif sm:5xl">TrackEx</h1>
+        <div className="flex justify-between items-center mb-6">
+          <h1 className="text-2xl font-bold"> Hi User 👋</h1>
+
+          <LogOut />
+        </div>
+
+        <ExpenseForm />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
