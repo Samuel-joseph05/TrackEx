e40e765e2f49872422c054b461eba3d291ae0dab
Samuel-joseph05 <samuela05022004@gmail.com>
2025-10-08 12:02:37 +0530

    fix(ui): update navigation on successful login and improve layout of Login form

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -18,7 +18,7 @@ function Login() {
       );
 
       if (res.data.login === true) {
-        navigate("/"); // redirect to ExpenseForm
+        navigate("/dashboard"); // redirect to home
       } else {
         alert(res.data.message || "Login failed");
       }
@@ -28,9 +28,12 @@ function Login() {
     }
   };
 
+  const handleback=()=>{
+    window.history.back();
+  }
   return (
     <div className="flex justify-center">
-      <form onSubmit={handleSubmit} className="flex flex-col gap-3 p-5 shadow-xl rounded-3xl w-96">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-3 ml-10  mr-10  mt-10 p-10 shadow-xl rounded-3xl w-96">
         <h1 className="text-2xl">Login</h1>
         <label>Email</label>
         <input
@@ -48,17 +51,23 @@ function Login() {
           required
           className="p-2 rounded-xl bg-gray-100 outline-none"
         />
-        <button type="submit" className="bg-purple-600 text-white p-2 rounded-xl hover:bg-purple-700">
+        <button type="submit" className="bg-purple-600 text-white p-2 rounded-xl hover:bg-purple-700  hover:cursor-pointer">
           Sign In
         </button>
         <button
           type="button"
           onClick={() => navigate("/register")}
-          className="bg-gray-300 text-black p-2 rounded-xl hover:bg-sky-400"
+          className="bg-gray-300 text-black p-2 rounded-xl hover:bg-sky-400  hover:cursor-pointer"
         >
           Sign Up
         </button>
+        <div className="mt-3">
+          <a className="text-black hover:underline hover:cursor-pointer" onClick={handleback}>back</a>
+       
+        </div>
+      
       </form>
+      
     </div>
   );
 }
