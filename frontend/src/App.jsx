02dc18b05d20de433f37b436d41a5e2bd2d83133
Samuel-joseph05 <samuela05022004@gmail.com>
2025-10-08 12:01:14 +0530

    feat(ui): update routing to include Home component and enhance NotFoundPage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,16 +6,25 @@ import Login from './pages/Login'
 import Dashboard from './pages/Dashboard'
 import ExpenseForm from './components/ExpenseForm'
 import ExpenseList from './components/ExpenseList'
+import Home from './pages/Home'
 
 function NotFoundPage() {
-  return <h1>404 - Page Not Found</h1>
-}
+  return(
+  <div>
+    <h1 className='text-center mx-10 my-6 font-bold
+      p-10  '>404 - Page Not Found😓</h1>  
+     <a href="/" className="flex justify-center text-blue-600 hover:cursor-pointer hover:underline">Go Home</a>
+     </div>
+     );
+
+} 
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ExpenseForm />} />
+         <Route path="/" element={<Home />} />
+        <Route path="/expenseform" element={<ExpenseForm />} />
         <Route path="/expenselist" element={<ExpenseList />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
