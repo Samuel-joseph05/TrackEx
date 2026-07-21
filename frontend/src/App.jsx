5af3cf318f801704728eceff85818a2d09c43e8c
Samuel-joseph05 <samuela05022004@gmail.com>
2026-07-21 18:22:12 +0530

    refactor(App): improve code formatting and structure for readability

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,38 +1,61 @@
-
-import './index.css'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import Register from './pages/Register'
-import Login from './pages/Login'
-import Dashboard from './pages/Dashboard'
-import ExpenseForm from './components/ExpenseForm'
-import ExpenseList from './components/ExpenseList'
-import Home from './pages/Home'
+import "./index.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseList from "./components/ExpenseList";
+import Home from "./pages/Home";
+import { Toaster } from "sonner";
 
 function NotFoundPage() {
-  return(
-  <div>
-    <h1 className='text-center mx-10 my-6 font-bold
-      p-10  '>404 - Page Not Found😓</h1>  
-     <a href="/" className="flex justify-center text-blue-600 hover:cursor-pointer hover:underline">Go Home</a>
-     </div>
-     );
-
-} 
+  return (
+    <div>
+      <h1
+        className="text-center mx-10 my-6 font-bold
+      p-10  "
+      >
+        404 - Page Not Found😓
+      </h1>
+      <a
+        href="/"
+        className="flex justify-center text-blue-600 hover:cursor-pointer hover:underline"
+      >
+        Go Home
+      </a>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <Router>
-      <Routes>
-         <Route path="/" element={<Home />} />
-        <Route path="/expenseform" element={<ExpenseForm />} />
-        <Route path="/expenselist" element={<ExpenseList />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </Router>
-  )
+    <>
+      <Toaster
+        position="top-right"
+        richColors
+        expand={true}
+        closeButton
+        duration={1000}
+        visibleToasts={3}
+        toastOptions={{
+          style: {
+            maxWidth: "90vw",
+          },
+        }}
+      />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/expenseform" element={<ExpenseForm />} />
+          <Route path="/expenselist" element={<ExpenseList />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
